80e04f7d0e4fc304b350229d7e943a7702cf66ae
K N P <perik@us.ibm.com>
2025-03-05 07:50:03 -0500

    Update log-generator.go

--- log-generator.go
+++ log-generator.go
@@ -4,12 +4,6 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
-	"os"
-	"strconv"
-	"net/http"
-	"context"
-	"syscall"
-	"os/signal"
 )
 
 func Debug(format string, args ...interface{}) {
@@ -61,15 +55,6 @@ func main() {
 	colorArray := []string{"red", "green", "yellow", "blue", "magenta", "cyan", "gray", "white"}
 	logLevels := []string{"CRITICAL", "DEBUG", "EMERGENCY", "ERROR", "FATAL", "INFO", "SEVERE", "TRACE", "WARN", "ALERT"}
 
-	ctx := context.Background()
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
-
-	srv := &http.Server{Addr: ":8080"}
-
-	// Debug the http handler for all requests
-	// http.HandleFunc("/", HandleHTTP)
-
 	// Seed the random number generator
 	rand.Seed(time.Now().UnixNano())
 
@@ -83,28 +68,4 @@ func main() {
 		randomLevel := rand.Intn(len(logLevels))
 		printColor (colorArray[randomColor], logLevels[randomLevel])
 	}
-
-	if delay := os.Getenv("HTTP_DELAY"); delay != "" {
-			if sec, _ := strconv.Atoi(delay); sec != 0 {
-				Debug("Sleeping %d seconds", sec)
-				time.Sleep(time.Duration(sec) * time.Second)
-			}
-		}
-
-		go func() {
-			
-			Debug("An instance of application '%s' has been started :)", os.Getenv("CE_APP"))
-			Debug("Listening on port 8080")
-
-			if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-				Debug ("failed to start server: %v", err)
-			}
-		}()
-
-		<-signals
-		Debug("shutting down server")
-		if err := srv.Shutdown(ctx); err != nil {
-			Debug ("failed to shutdown server: %v", err)
-		}
-		Debug("shutdown done")
 }
